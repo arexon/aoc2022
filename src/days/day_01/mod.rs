f3020718663f8c538b439e086b1427561d15842b
arexon <arexonreal@gmail.com>
2022-12-03 20:51:39 +0200

    refactor: split day 01 parts

--- src/days/day_01/mod.rs
+++ src/days/day_01/mod.rs
@@ -1,32 +1,56 @@
 pub fn run(input: &str) -> (String, String) {
     let input: Vec<&str> = input.split("\n\n").collect();
-    let mut input: Vec<u32> = input
-        .iter()
-        .map(|bag| {
-            bag.split('\n')
-                .map(|calo| calo.parse::<u32>().unwrap())
-                .sum()
-        })
-        .collect();
-    input.sort_by(|a, b| b.cmp(a));
-    let mut input = input.iter();
 
     (
-        input.next().unwrap().to_string(),
-        input.take(3).sum::<u32>().to_string(),
+        part_one(input.clone()).to_string(),
+        part_two(input).to_string(),
     )
+}
+
+fn part_one(input: Vec<&str>) -> u32 {
+    let mut input: Vec<u32> = input
+        .iter()
+        .map(|s| s.split('\n').map(|s| s.parse::<u32>().unwrap()).sum())
+        .collect();
+    input.sort_by(|a, b| b.cmp(a));
+    *input.first().unwrap()
+}
+
+fn part_two(input: Vec<&str>) -> u32 {
+    let mut input: Vec<u32> = input
+        .iter()
+        .map(|s| s.split('\n').map(|s| s.parse::<u32>().unwrap()).sum())
+        .collect();
+    input.sort_by(|a, b| b.cmp(a));
+    input.iter().take(3).sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::run;
-    use crate::Solution;
+    use super::{part_one, part_two};
+
+    const INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
 
     #[test]
     fn test_part_one() {
-        assert_eq!(
-            run(&Solution::new("01").input),
-            ("66306".to_string(), "193216".to_string())
-        );
+        assert_eq!(part_one(INPUT.split("\n\n").collect()), 24000);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(INPUT.split("\n\n").collect()), 45000);
     }
 }
