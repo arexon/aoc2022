da508c19562a899c78d8647522c1ed54238299b4
arexon <arexonreal@gmail.com>
2022-12-03 20:55:43 +0200

    feat: add day 00 as a template

---
+++ src/days/day_00/mod.rs
@@ -0,0 +1,28 @@
+pub fn run(input: &str) -> (String, String) {
+    (part_one().to_string(), part_two().to_string())
+}
+
+fn part_one() -> u32 {
+    0
+}
+
+fn part_two() -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one(), 0);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(), 0);
+    }
+}
